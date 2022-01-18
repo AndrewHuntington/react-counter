c65cff0421b291e27889c974abe9927a67232838
Andrew Huntington <andrew.huntington@gmail.com>
2022-01-18 10:13:15 +0900

    Create counter with start/stop functionallity. Add some unit testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
+test('timer begins at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const timer = screen.getByText(/0/);
+  expect(timer).toBeInTheDocument();
+});
+
+test('renders start button', () => {
+  render(<App />);
+  const startBtn = screen.getByText(/start/i);
+  expect(startBtn).toBeInTheDocument();
+});
+
+test('renders pause button', () => {
+  render(<App />);
+  const pauseBtn = screen.getByText(/pause/i);
+  expect(pauseBtn).toBeInTheDocument();
+});
+
+test('renders reset button', () => {
+  render(<App />);
+  const resetBtn = screen.getByText(/reset/i);
+  expect(resetBtn).toBeInTheDocument();
+});
+
+test('counter begins on start press', async () => {
+  await act(async () => {
+    render(<App />);
+    const startBtn = screen.getByText(/start/i);
+    fireEvent.click(startBtn);
+
+    await new Promise((res) => setTimeout(() => res('done'), 1500));
+    const counter = await waitFor(() => screen.findByRole('timer'));
+    expect(counter).toHaveTextContent('1');
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [isRunning]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="counter-display">
+        <h1 className="counter" role="timer">
+          {seconds}
+        </h1>
+      </div>
+      <div className="buttons">
+        <div className="start-btn-wrapper bottom-margin">
+          <button className="start-btn" onClick={() => setIsRunning(true)}>
+            START
+          </button>
+        </div>
+        <div className="pause-btn-wrapper bottom-margin">
+          <button
+            className="pause-btn"
+            onClick={() => {
+              setIsRunning(!isRunning);
+            }}
+          >
+            PAUSE
+          </button>
+        </div>
+        <div className="reset-btn-wrapper">
+          <button className="reset-btn">RESET</button>
+        </div>
+      </div>
     </div>
   );
 }
