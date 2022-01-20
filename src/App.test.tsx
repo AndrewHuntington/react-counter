274f80e6af45f1e55bd08545cda17c9fc65bb9b8
Andrew Huntington <andrew.huntington@gmail.com>
2022-01-20 18:46:13 +0900

    Make counter role over into minutes after 60 seconds. Update tests to reflect new functionality

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,16 +9,28 @@ test('timer begins at 0', () => {
   expect(timer).toBeInTheDocument();
 });
 
-test('renders start button', () => {
+test('renders start button on page load', () => {
   render(<App />);
   const startBtn = screen.getByTestId('start-btn');
   expect(startBtn).toBeInTheDocument();
 });
 
-test('renders pause button', () => {
+test('does not render pause button before timer start', () => {
   render(<App />);
+  const pauseBtn = screen.queryByTestId('pause-btn');
+  expect(pauseBtn).not.toBeInTheDocument();
+});
+
+test('renders pause btn after start btn is clicked', async () => {
+  render(<App />);
+
+  const startBtn = screen.getByTestId('start-btn');
+  expect(startBtn).toBeInTheDocument();
+  fireEvent.click(startBtn);
+
   const pauseBtn = screen.getByTestId('pause-btn');
   expect(pauseBtn).toBeInTheDocument();
+  fireEvent.click(pauseBtn);
 });
 
 test('renders reset button', () => {
@@ -43,12 +55,18 @@ test('counter begins on start press', async () => {
 test('pause button pauses count', async () => {
   jest.useFakeTimers();
   render(<App />);
-  const pauseBtn = screen.getByTestId('pause-btn');
-  fireEvent.click(pauseBtn);
+
+  const startBtn = screen.getByTestId('start-btn');
+  expect(startBtn).toBeInTheDocument();
+  fireEvent.click(startBtn);
 
   act(() => {
     jest.advanceTimersByTime(2500);
   });
+
+  const pauseBtn = screen.getByTestId('pause-btn');
+  expect(pauseBtn).toBeInTheDocument();
+  fireEvent.click(pauseBtn);
 
   const counter = await waitFor(() => screen.findByRole('timer'));
   expect(counter).toHaveTextContent('2');
@@ -60,4 +78,17 @@ test('reset button sets counter to 0', async () => {
   fireEvent.click(resetBtn);
   const counter = await waitFor(() => screen.findByRole('timer'));
   expect(counter).toHaveTextContent('0');
+});
+
+test('rolls over to 1 minute on 60 seconds', async () => {
+  jest.useFakeTimers();
+  render(<App />);
+  const startBtn = screen.getByTestId('start-btn');
+  fireEvent.click(startBtn);
+
+  act(() => {
+    jest.advanceTimersByTime(60500);
+  });
+  const counter = await waitFor(() => screen.findByRole('timer'));
+  expect(counter).toHaveTextContent('1:00');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -18,27 +18,35 @@ function App() {
     <div className="text-center">
       <div className="">
         <h1 className="text-8xl" role="timer">
-          {seconds}
+          {seconds < 60
+            ? seconds
+            : `${Math.floor(seconds / 60)}:${(seconds % 60)
+                .toString()
+                .padStart(2, '0')}`}
         </h1>
       </div>
       <div className="">
-        <button
-          data-testid="start-btn"
-          className="mr-5"
-          onClick={() => setIsRunning(true)}
-        >
-          <i className="fa-solid fa-play fa-2x"></i>
-        </button>
+        {isRunning ? null : (
+          <button
+            data-testid="start-btn"
+            className="mr-5"
+            onClick={() => setIsRunning(true)}
+          >
+            <i className="fa-solid fa-play fa-2x"></i>
+          </button>
+        )}
 
-        <button
-          data-testid="pause-btn"
-          className="mr-5"
-          onClick={() => {
-            setIsRunning(!isRunning);
-          }}
-        >
-          <i className="fa-solid fa-pause fa-2x"></i>
-        </button>
+        {isRunning ? (
+          <button
+            data-testid="pause-btn"
+            className="mr-5"
+            onClick={() => {
+              setIsRunning(!isRunning);
+            }}
+          >
+            <i className="fa-solid fa-pause fa-2x"></i>
+          </button>
+        ) : null}
 
         <button
           data-testid="reset-btn"
